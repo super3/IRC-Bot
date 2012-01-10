3110fe41e934cbd78dddc66a41c04ffc38b231c0
Shawn Wilkinson <super3boy@gmail.com>
2012-01-09 21:16:35 -0500

    edited poke docs

--- Classes/Command/Poke.php
+++ Classes/Command/Poke.php
@@ -2,8 +2,8 @@
 // Namespace
 namespace Command;
 //Sends the arguments to the channel, like say from a user.
-// arguments[0] == Channel or User to say message to.
-// arguments[1] == Message text.
+// arguments[0] == Channel or User to send Poke to.
+// arguments[1] == Poke Victim.
 class Poke extends \Library\IRCCommand {
     public function command() {
         $this->IRCBot->sendDataToServer( 'PRIVMSG ' . $this->arguments[0] . ' :'. chr(1). 'ACTION pokes '.

e125bbbdaa5fe1b1141aca06758f97c12f70f5a0
Shawn Wilkinson <super3boy@gmail.com>
2012-02-14 19:48:01 -0500

    Remove Moved Files

--- footer.php
+++
@@ -1,8 +0,0 @@
-    </div> <!-- /row -->
-</div> <!-- /container -->
-<!-- Javascript -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="Style/jquery.js"></script>
-<script src="Style/bootstrap-dropdown.js"></script>
-</body>
-</html>

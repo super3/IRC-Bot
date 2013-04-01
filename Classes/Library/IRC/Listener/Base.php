34747df8628de1e6b204a8a2fff980227cdf6e6f
superaktieboy <superaktieboy@gmail.com>
2013-04-01 03:16:41 +0100

    Refactored code to have less repetition.
    Change-Id: I5e96b72cc8ac3a714ab728c1c362963000919d79

--- Classes/Library/IRC/Listener/Base.php
+++ Classes/Library/IRC/Listener/Base.php
@@ -16,28 +16,4 @@ abstract class Base extends \Library\IRC\Base
      * @return array
      */
     abstract function getKeywords();
-
-    /**
-     * Returns the raw data as an array of parsed data.
-     * @param string $data
-     * @return array
-     */
-    protected function getArguments($data) {
-        $args = explode( ' ', $data );
-        $func = function($value) {
-            return trim( \Library\FunctionCollection::removeLineBreaks( $value ) );
-        };
-        
-        // Strip leading colons off messages (that are meant to be seen by IRC only), only look at   //
-        // the first three elements to avoid cutting off a user who starts an argument with a colon. //
-        // Corrects :test!example@example.com PRIVMSG #cs :foo bar :baz ::: to                       //
-        //           test!example@example.com PRIVMSG #cs  foo bar :baz :::                          //
-        // This is useful because it means listener writers don't have to manually remove these      //
-        // characters (which is what Joins.php was doing).                                           //
-        for($i = 0; $i <= (((sizeof($args) - 1 ) < 3) ? (sizeof($args) -1) : 3); $i++)
-            $args[$i] = (strpos($args[$i], ":") === 0) ? substr($args[$i], 1) : $args[$i];
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-
-        return array_map($func, $args);
-    }
 }

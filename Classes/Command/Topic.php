aedb3c81be1d28607dcc68d144e8a71c3c0d8ecf
Tim Vos <TimTims@users.noreply.github.com>
2015-01-05 23:14:28 +0000

    Added Topic Change Plugin

---
+++ Classes/Command/Topic.php
@@ -0,0 +1,46 @@
+<?php
+// Namespace
+namespace Command;
+
+/**
+ * Sets the Topic of the Channel.
+ * arguments[0] == Channel or User to say message to.
+ *
+ * @package IRCBot
+ * @subpackage Command
+ * @author Tim Vos <timtimss@outlook.com>
+ */
+class Topic extends \Library\IRC\Command\Base {
+    /**
+     * The command's help text.
+     *
+     * @var string
+     */
+    protected $help = '!topic';
+
+    /**
+     * Verify the user before executing this command.
+     *
+     * @var bool
+     */
+    protected $verify = true;
+
+    /**
+     * The number of arguments the command needs.
+     *
+     * @var integer
+     */
+    protected $numberOfArguments = -1;
+
+    /**
+     * Sets the selected channel topic
+     */
+    public function command() {
+
+		if(strpos($this->arguments[0],'#') !== false){
+			$this->connection->sendData('TOPIC ' . $this->arguments[0] . " :" . implode( ' ', array_slice( $this->arguments, 1 ) ));
+		} else {
+			$this->connection->sendData('TOPIC ' . $this->source . " :" . implode(' ', $this->arguments));
+		}
+    }
+}
